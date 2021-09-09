c0592d3a7926e1be004ffeb9d37238e2f1713234
aljucodes <aljithmadambichonedan@gmail.com>
2021-09-09 12:51:05 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'flutter_assingnment',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _mainText = "click on the button";
+
+  void _changeText() {
+    setState(() {
+      _mainText = " text is changed now ";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _mainText,
+            ),
+            // ignore: deprecated_member_use
+            RaisedButton(
+                onPressed: _changeText, child: const Text("press here ")),
+          ],
+        ),
+      ),
+    );
+  }
+}
